621170e1dc34b7c1ce38bbb666ebcfe7607fe0ab
alicao16 <cinaalicao06@gmail.com>
2026-03-10 09:58:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -651,7 +651,7 @@ with st.sidebar:
     alpha_value = st.slider(
     "Price elasticity (alpha)",
     min_value=0.005,
-    max_value=0.05,
+    max_value=0.050,
     value=st.session_state.get("alpha", 0.02),
     step=0.005,
     help="Higher value = demand reacts more strongly to price"
