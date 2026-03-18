f402d274d16a3e25cd153c9250cbec5d97a86d57
alicao16 <cinaalicao06@gmail.com>
2026-03-18 13:15:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -583,12 +583,12 @@ def generate_bookings(booking_date):
             season_factor = 1.0
 n0 = n0 * season_factor  # applicato alla domanda di base
         # ===== PARAMETRI DOMANDA =====
-        n0 = st.session_state.get("market_demand", 10)
-        p0 = st.session_state.get("p0", 100)
-        with st.sidebar:
-            st.divider()
-            st.subheader("📈 Demand Curve Steepness")
-        alpha = st.session_state.get("alpha", 0.03)
+    n0 = st.session_state.get("market_demand", 10)
+    p0 = st.session_state.get("p0", 100)
+    with st.sidebar:
+        st.divider()
+        st.subheader("📈 Demand Curve Steepness")
+    alpha = st.session_state.get("alpha", 0.03)
             
         
         C = st.session_state.total_rooms
